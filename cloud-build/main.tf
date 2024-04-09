b0abb0922838f31c0962dadf0cb779325a0b66b4
Snuffules <53743351+Snuffules@users.noreply.github.com>
2024-04-09 10:43:16 +0300

    Create main.tf

---
+++ cloud-build/main.tf
@@ -0,0 +1,13 @@
+provider "google" {
+  project     = "vladimir-europe-cloud-tests"
+}
+
+data "terraform_remote_state" "db" {
+  backend = "gcs"
+  config = {
+    # Replace this with your bucket name!
+    bucket  = "snf-gcp-tf-state"
+    #key    = "terraform/state/default.tfstate"
+    #region = "us-east-2"
+  }
+}
