a6e4a008c80ea6458ec2901f4a651936f2a9b5cb
Snuffules <53743351+Snuffules@users.noreply.github.com>
2024-04-09 14:59:20 +0300

    Update main.tf

--- cloud-build/main.tf
+++ cloud-build/main.tf
@@ -1,35 +1,34 @@
-provider "google" {
-  project     = "vladimir-europe-cloud-tests"
-  region  = "europe-west-3"
-  zone    = "europe-west-3-b"
-}
-
 data "terraform_remote_state" "db" {
   backend = "gcs"
   config = {
     # Replace this with your bucket name!
-    bucket  = "snf-gcp-tf-state"
+    bucket = "snf-gcp-tf-state"
     #key    = "terraform/state/default.tfstate"
     #region = "europe-west-3"
   }
 }
 
-resource "google_project_service" "compute_service" {
-  project = local.project_id
-  service = "compute.googleapis.com"
-}
 resource "google_compute_instance" "vm_instance" {
   name         = "solana-node"
   machine_type = "n2d-highmem-64"
+
+  network_interface {
+    network    = "network1"
+    subnetwork = google_compute_subnetwork.network1_subnet1.name
+
+    access_config {
+      // Ephemeral IP. Leave this block empty if you don't want to assign a public IP.
+    }
+  }
 
   tags = ["solana-node"]
 
   boot_disk {
     initialize_params {
       image = "ubuntu-2004-focal-v20230417"
-      size  = 2000  # Disk size in GB
+      size  = 2000 # Disk size in GB
       type  = "pd-ssd"
     }
   }
-
+}
 
