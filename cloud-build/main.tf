34c2212223c072108e30e48d5cdff5422a8f14e3
Snuffules <53743351+Snuffules@users.noreply.github.com>
2024-04-09 11:57:37 +0300

    Update main.tf

--- cloud-build/main.tf
+++ cloud-build/main.tf
@@ -12,17 +12,4 @@ data "terraform_remote_state" "db" {
   }
 }
 
-resource "google_compute_network" "network1" {
-  project  = "vladimir-europe-cloud-tests" 
-  name                    = "network1"
-  auto_create_subnetworks = false
-}
 
-resource "google_compute_subnetwork" "network1_subnet1" {
-  project  = "vladimir-europe-cloud-tests" 
-
-  name          = "solana-node-subnet-1"
-  ip_cidr_range = "10.0.1.0/24"
-  region        = "europe-west-3"
-  network       = google_compute_network.network1.id
-}
