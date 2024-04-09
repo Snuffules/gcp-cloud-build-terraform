a0f1ea658de7a13fc6b6537064256d357143b8fe
Snuffules <53743351+Snuffules@users.noreply.github.com>
2024-04-09 14:59:37 +0300

    Update providers.tf

--- cloud-build/providers.tf
+++ cloud-build/providers.tf
@@ -2,3 +2,9 @@ resource "google_project_service" "compute_service" {
   project = "vladimir-europe-cloud-tests"
   service = "compute.googleapis.com"
 }
+
+provider "google" {
+  project     = "vladimir-europe-cloud-tests"
+  region  = "europe-west-3"
+  zone    = "europe-west-3-b"
+}
