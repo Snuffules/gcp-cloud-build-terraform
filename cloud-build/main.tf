e76aed92af32f599b387ecf5b172c82b57459433
Snuffules <53743351+Snuffules@users.noreply.github.com>
2024-04-09 12:59:55 +0300

    Update main.tf

--- cloud-build/main.tf
+++ cloud-build/main.tf
@@ -1,5 +1,7 @@
 provider "google" {
   project     = "vladimir-europe-cloud-tests"
+  region  = "europe-west-3"
+  zone    = "europe-west-3-b"
 }
 
 data "terraform_remote_state" "db" {
@@ -11,5 +13,23 @@ data "terraform_remote_state" "db" {
     #region = "europe-west-3"
   }
 }
+
+resource "google_project_service" "compute_service" {
+  project = local.project_id
+  service = "compute.googleapis.com"
+}
+resource "google_compute_instance" "vm_instance" {
+  name         = "solana-node"
+  machine_type = "solana-node"
+
+  tags = ["solana-node"]
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-2004-focal-v20230417"
+      size  = 2000  # Disk size in GB
+      type  = "pd-ssd"
+    }
+  }
 
 
