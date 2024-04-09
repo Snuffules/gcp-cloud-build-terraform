9c1a7403a39e8ff72278876704c8f0f1c8b51ba2
Snuffules <53743351+Snuffules@users.noreply.github.com>
2024-04-09 14:59:56 +0300

    Update vpc.tf

--- cloud-build/vpc.tf
+++ cloud-build/vpc.tf
@@ -1,11 +1,11 @@
 resource "google_compute_network" "network1" {
-  project  = "vladimir-europe-cloud-tests" 
+  project                 = "vladimir-europe-cloud-tests"
   name                    = "network1"
   auto_create_subnetworks = false
 }
 
 resource "google_compute_subnetwork" "network1_subnet1" {
-  project  = "vladimir-europe-cloud-tests" 
+  project = "vladimir-europe-cloud-tests"
 
   name          = "solana-node-subnet-1"
   ip_cidr_range = "10.0.1.0/24"
